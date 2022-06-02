e43126c11c444c0929b036a47e6279ba938d049d
dennis <wakahiad@gmail.com>
2022-06-02 15:18:04 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stripe Menus Demo
+
+Application developed in React js
+
+Click [here](https://stripe-menus-demo.netlify.app) to visit application
+
+Dennis Githinji 🚀️
--- src/context.js
+++ src/context.js
@@ -1,2 +1,50 @@
-import React, { useState, useContext } from 'react'
+import React, {useState, useContext, createContext} from 'react'
 import sublinks from './data'
+
+const AppContext = createContext();
+
+export const AppProvider = ({children}) => {
+
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({page: '', links: []})
+
+    const openSideBar = () => {
+        setIsSideBarOpen(true)
+    }
+
+    const closeSideBar = () => {
+        setIsSideBarOpen(false)
+    }
+
+    const openSubMenu = (text, coordinates) => {
+        const page = sublinks.find((subLink) => subLink.page === text);
+        setPage(page)
+        setLocation(coordinates)
+        setIsSubMenuOpen(true)
+    }
+
+    const closeSubMenu = () => {
+        setIsSubMenuOpen(false)
+    }
+
+    return (
+        <AppContext.Provider value={{
+            isSubMenuOpen,
+            isSideBarOpen,
+            openSideBar,
+            closeSideBar,
+            openSubMenu,
+            closeSubMenu,
+            location,
+            page
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
